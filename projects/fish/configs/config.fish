afd90e549f77ac7d17be7a5209a72b5782f9744a
Yoshizawa Shogo <shogo.yoshizawa.2000@gmail.com>
2022-09-12 14:31:40 +0900

    fish環境の調整: 日本語に対応

--- projects/fish/configs/config.fish
+++ projects/fish/configs/config.fish
@@ -1,8 +1,9 @@
-set -g -x fish_greeting ''
+export LANG=ja_JP.UTF-8
+export LC_CTYPE=ja_JP.UTF-8
 
 if status --is-interactive
+  set -g -x fish_greeting ''
   date
-  
   sensors
   uptime
   free -h
