feae449d548aee83a622332b79c9104c1f957589
Yoshizawa Shogo <shogo.yoshizawa.2000@gmail.com>
2022-09-13 13:24:12 +0900

    fishのプロンプトに表示されるpathが省略されないように変更

--- projects/fish/configs/functions/fish_prompt.fish
+++ projects/fish/configs/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt
-    printf '%s%s@%s%s%s%s%s' (set_color yellow) $USER (set_color purple) (prompt_hostname) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) 
+    printf '%s%s@%s%s%s%s%s' (set_color yellow) $USER (set_color purple) (prompt_hostname) (set_color $fish_color_cwd) (pwd | string replace $HOME '~') (set_color normal) 
     printf '%s' (fish_git_prompt)
     printf ':[%s]\n> ' (date +%X)
 end
